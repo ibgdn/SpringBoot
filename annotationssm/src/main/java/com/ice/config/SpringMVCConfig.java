038635ac4ca05eebd972cd671d2156f636a48642
ibgdn <ibgdn4@gmail.com>
2020-04-23 22:20:16 +0800

    注解项目添加静态资源、拦截器及 json 数据序列化示例。

--- annotationssm/src/main/java/com/ice/config/SpringMVCConfig.java
+++ annotationssm/src/main/java/com/ice/config/SpringMVCConfig.java
@@ -1,14 +1,56 @@
 package com.ice.config;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.ice.interceptor.MyInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * Spring Mvc 的配置文件，相当于 spring-servlet.xml
+ * <p>
+ * 通过继承 WebMvcConfigurationSupport。
  */
 @Configuration
 @ComponentScan(basePackages = "com.ice", useDefaultFilters = false, includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Controller.class), @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)})
-public class SpringMVCConfig {
+public class SpringMVCConfig extends WebMvcConfigurationSupport {
+    /*
+        添加静态资源过滤
+    */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    /*
+        添加拦截器
+    */
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        // "/**" 代表任意多层路径
+        registry.addInterceptor(myInterceptor()).addPathPatterns("/**");
+    }
+
+    @Bean
+    MyInterceptor myInterceptor() {
+        return new MyInterceptor();
+    }
+    // 添加拦截器 end
+
+    /*
+        json 序列化与反序列化
+    */
+    @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        converters.add(converter);
+    }
 }
--- annotationssm/src/main/java/com/ice/controller/HelloController.java
+++ annotationssm/src/main/java/com/ice/controller/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloController {
     @Autowired
@@ -13,5 +16,15 @@ public class HelloController {
     @GetMapping(value = "/hello", produces = "text/html;charset=utf-8")
     public String hello() {
         return helloService.sayHello();
+    }
+
+    @GetMapping(value = "/jsonData")
+    public List<String> data() {
+        int length = 5;
+        ArrayList<String> list = new ArrayList<String>(length);
+        for (int i = 0; i < length; i++) {
+            list.add("for i >>> " + i);
+        }
+        return list;
     }
 }
