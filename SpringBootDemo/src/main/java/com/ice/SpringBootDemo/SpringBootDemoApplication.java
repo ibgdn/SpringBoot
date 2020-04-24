1dd9a3f86577927fd443dccace37b8af19cf7d30
ibgdn <ibgdn4@gmail.com>
2020-04-24 23:35:25 +0800

    自定义 banner .

--- SpringBootDemo/src/main/java/com/ice/SpringBootDemo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/ice/SpringBootDemo/SpringBootDemoApplication.java
@@ -1,13 +1,22 @@
 package com.ice.SpringBootDemo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+/*
+		// 项目启动过程中禁用 banner start
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(SpringBootDemoApplication.class);
+		builder.bannerMode(Banner.Mode.OFF);
+		builder.run(args);
+		// 项目启动过程中禁用 banner end
+*/
 	}
 
 }
