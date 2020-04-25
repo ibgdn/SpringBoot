c22b4775bf17b3f1743c18f0d02415e916e8600f
ibgdn <ibgdn4@gmail.com>
2020-04-25 11:06:25 +0800

    Spring 属性注入。

---
+++ SpringBootDemo/src/main/java/com/ice/SpringBootDemo/properties/Book.java
@@ -0,0 +1,53 @@
+package com.ice.SpringBootDemo.properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * 实体类，测试 Spring 属性注入
+ */
+@Component
+@PropertySource("classpath:book.properties")
+public class Book {
+
+    @Value("${book.id}")
+    public String id;
+    @Value("${book.name}")
+    public String name;
+    @Value("${book.author}")
+    public String author;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
--- SpringBootDemo/src/test/java/com/ice/SpringBootDemo/SpringBootDemoApplicationTests.java
+++ SpringBootDemo/src/test/java/com/ice/SpringBootDemo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.ice.SpringBootDemo;
 
+import com.ice.SpringBootDemo.properties.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
+	@Autowired
+	Book book;
 
 	@Test
 	void contextLoads() {
+		// 测试 Spring 属性注入
+		System.out.println(book);
 	}
 
 }
