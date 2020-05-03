11396506e403b7fbbf3a9f4364a40c20f402bccc
ibgdn <ibgdn4@gmail.com>
2020-05-03 22:44:50 +0800

    SpringBoot 注册拦截器。

---
+++ SpringBootWeb/src/main/java/com/ice/interceptor/CustomInterceptor.java
@@ -0,0 +1,28 @@
+package com.ice.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 19.Spring Boot 中注册拦截器
+ */
+public class CustomInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("Interceptor preHandle.");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("Interceptor postHandle.");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("Interceptor afterCompletion.");
+    }
+}
