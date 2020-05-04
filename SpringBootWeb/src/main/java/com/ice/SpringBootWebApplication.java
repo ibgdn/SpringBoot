52687dbfa5957ecbb8d6f2c70b606273f0bf1cb9
ibgdn <ibgdn4@gmail.com>
2020-05-04 20:06:31 +0800

    SpringBoot 去除自动化配置（主启动类或配置文件）。

--- SpringBootWeb/src/main/java/com/ice/SpringBootWebApplication.java
+++ SpringBootWeb/src/main/java/com/ice/SpringBootWebApplication.java
@@ -3,8 +3,10 @@ package com.ice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+// 取消自动化配置，除了可以在 resources/application.properties 里配置，也可以在这里配置
+@SpringBootApplication(exclude = WebMvcConfigurer.class)
 @ServletComponentScan(basePackages = "com.ice.servlet")
 public class SpringBootWebApplication {
 
