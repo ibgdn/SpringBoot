6cbb5ea61380c41b61287835444c65900ed84ca3
ibgdn <ibgdn4@gmail.com>
2020-05-14 23:34:48 +0800

    SpringBoot REST 自定义查询方法。

--- SpringBootREST/src/main/java/com/ice/jparest/dao/BookDao.java
+++ SpringBootREST/src/main/java/com/ice/jparest/dao/BookDao.java
@@ -2,9 +2,17 @@ package com.ice.jparest.dao;
 
 import com.ice.jparest.bean.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 /**
  * 2.搭建 Jpa 环境
  */
+@RepositoryRestResource(path = "fbs", collectionResourceRel = "bs", itemResourceRel = "b")
 public interface BookDao extends JpaRepository<Book, Integer> {
+    @RestResource(path = "byName", rel = "findByName")
+    List<Book> findBookByNameContaining(@Param("name") String name);
 }
