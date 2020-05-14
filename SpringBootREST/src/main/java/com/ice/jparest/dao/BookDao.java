f086767232c175b77a01e22e83695a21d8d1d99a
ibgdn <ibgdn4@gmail.com>
2020-05-14 23:38:24 +0800

    SpringBoot REST 配置 CORS。

--- SpringBootREST/src/main/java/com/ice/jparest/dao/BookDao.java
+++ SpringBootREST/src/main/java/com/ice/jparest/dao/BookDao.java
@@ -5,12 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 /**
  * 2.搭建 Jpa 环境
  */
+@CrossOrigin
 @RepositoryRestResource(path = "fbs", collectionResourceRel = "bs", itemResourceRel = "b")
 public interface BookDao extends JpaRepository<Book, Integer> {
     @RestResource(path = "byName", rel = "findByName")
