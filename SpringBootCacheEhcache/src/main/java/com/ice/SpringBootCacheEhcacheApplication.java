490c4f524c5f5d5ef090fa1ecf1c9f25d12a5500
ibgdn <ibgdn4@gmail.com>
2020-05-18 00:25:38 +0800

    SpringBoot Cache 整合 Ehcache。

---
+++ SpringBootCacheEhcache/src/main/java/com/ice/SpringBootCacheEhcacheApplication.java
@@ -0,0 +1,13 @@
+package com.ice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCacheEhcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCacheEhcacheApplication.class, args);
+	}
+
+}
