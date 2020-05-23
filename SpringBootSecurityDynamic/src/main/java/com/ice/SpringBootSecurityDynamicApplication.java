58846d99fdce8dd250de1056375eb63ee348fade
ibgdn <ibgdn4@gmail.com>
2020-05-23 11:27:33 +0800

    SpringBoot Security 动态配置权限初始化提交。

---
+++ SpringBootSecurityDynamic/src/main/java/com/ice/SpringBootSecurityDynamicApplication.java
@@ -0,0 +1,13 @@
+package com.ice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityDynamicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityDynamicApplication.class, args);
+	}
+
+}
