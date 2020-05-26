f346022097445d457c63fbbfeefe3f799ac48632
ibgdn <ibgdn4@gmail.com>
2020-05-26 22:46:42 +0800

    SpringBoot Security 结合 OAuth2 初始化提交。

---
+++ SpringBootSecurityOAuth2/src/test/java/com/ice/SpringBootSecurityOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
