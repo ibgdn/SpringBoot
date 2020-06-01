38beed9b341a33294e77f339fe6750440b24ba28
ibgdn <ibgdn4@gmail.com>
2020-06-01 22:59:58 +0800

    SpringBoot 整合 WebSocket 实现在线群聊初始化提交。

---
+++ SpringBootMultipleWebSocket/src/main/java/com/ice/SpringBootMultipleWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.ice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultipleWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultipleWebSocketApplication.class, args);
+	}
+
+}
