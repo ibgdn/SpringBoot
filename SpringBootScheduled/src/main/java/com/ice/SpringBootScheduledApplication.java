f89086cd6d8e0aeae800070b4bf2d7c017ccf361
ibgdn <ibgdn4@gmail.com>
2020-06-06 22:52:23 +0800

    SpringBoot 通过 Scheduled 实现定时任务初始化提交。

---
+++ SpringBootScheduled/src/main/java/com/ice/SpringBootScheduledApplication.java
@@ -0,0 +1,13 @@
+package com.ice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootScheduledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootScheduledApplication.class, args);
+	}
+
+}
