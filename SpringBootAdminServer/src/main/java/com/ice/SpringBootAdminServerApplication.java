f16b17a796dab6723306960db7e4ceea284597d3
ibgdn <ibgdn4@gmail.com>
2020-06-08 21:13:22 +0800

    SpringBoot 整合 Admin（server） 监控信息可视化初始化提交。

---
+++ SpringBootAdminServer/src/main/java/com/ice/SpringBootAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.ice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminServerApplication.class, args);
+	}
+
+}
