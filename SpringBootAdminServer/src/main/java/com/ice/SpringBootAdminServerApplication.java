35125a5ae064ef311a04c78e0092a0fce1cf7fd3
ibgdn <ibgdn4@gmail.com>
2020-06-08 21:35:08 +0800

    SpringBoot 整合 Admin（client、server） 监控信息可视化。

--- SpringBootAdminServer/src/main/java/com/ice/SpringBootAdminServerApplication.java
+++ SpringBootAdminServer/src/main/java/com/ice/SpringBootAdminServerApplication.java
@@ -1,13 +1,15 @@
 package com.ice;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SpringBootAdminServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAdminServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+    }
 
 }
